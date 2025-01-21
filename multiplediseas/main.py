89439b857f1a7ea058e8726a79479ab124cf42da
AryanMadkar <aryanmadkar70@gmail.com>
2025-01-21 08:04:44 +0530

    sending and accepting done to port

---
+++ multiplediseas/main.py
@@ -0,0 +1,156 @@
+from flask import Flask, request
+import numpy as np
+from processing.diabitiespocessing import scale_data_dibaties
+from processing.heartprocessing import scale_data_heart
+from processing.lungcancerprocessing import scale_data_lung
+from processing.brestcancerprocessing import scale_data_brest
+from model.loadmodel import (
+    diabitie_model,
+    heart_model,
+    lungcancer_model,
+    brest_cancer_model,
+    insurancemodel,
+)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return "Welcome to the Health App!"
+
+
+@app.route("/diabetes", methods=["POST"])
+def diabetes():
+    data = request.get_json()
+    pregnancies = data["pregnancies"]
+    glucose = data["glucose"]
+    bp = data["bp"]
+    skin_thickness = data["skin_thickness"]
+    insulin = data["insulin"]
+    bmi = data["bmi"]
+    dpf = data["dpf"]
+    age = data["age"]
+    input_data = [[pregnancies, glucose, bp, skin_thickness, insulin, bmi, dpf, age]]
+    inputarray = np.asarray(input_data).reshape(1, -1)
+    scaled_data, _ = scale_data_dibaties(inputarray)
+    prediction = diabitie_model.predict(scaled_data)
+    return {
+        "prediction": (
+            "Yes, you are likely to have diabetes."
+            if prediction[0] == 1
+            else "No, you are unlikely to have diabetes."
+        )
+    }
+
+
+@app.route("/heart", methods=["POST"])
+def heart():
+    data = request.get_json()
+    input_data = [
+        [
+            data["age"],
+            data["sex"],
+            data["cp"],
+            data["trestbps"],
+            data["chol"],
+            data["fbs"],
+            data["restecg"],
+            data["thalach"],
+            data["exang"],
+            data["oldpeak"],
+            data["slope"],
+            data["ca"],
+            data["thal"],
+        ]
+    ]
+    inputarray = np.asarray(input_data).reshape(1, -1)
+    scaled_data, _ = scale_data_heart(inputarray)
+    prediction = heart_model.predict(scaled_data)
+    return {
+        "prediction": (
+            "Yes, you are likely to have heart disease."
+            if prediction[0] == 1
+            else "No, you are unlikely to have heart disease."
+        )
+    }
+
+
+@app.route("/lungcancer", methods=["POST"])
+def lungcancer():
+    data = request.get_json()
+    input_data = [
+        [
+            data["gender"],
+            data["age"],
+            data["smoking"],
+            data["yellow_finger"],
+            data["anxiety"],
+            data["peer_pressure"],
+            data["chronic_disease"],
+            data["fatigue"],
+            data["allergy"],
+            data["wheezing"],
+            data["alcohol"],
+            data["coughing"],
+            data["shortness_of_breath"],
+            data["swallowing_difficulty"],
+            data["chest_pain"],
+        ]
+    ]
+    inputarray = np.asarray(input_data).reshape(1, -1)
+    scaled_data, _ = scale_data_lung(inputarray)
+    prediction = lungcancer_model.predict(scaled_data)
+    return {
+        "prediction": (
+            "Yes, you are likely to have lung cancer."
+            if prediction[0] == 1
+            else "No, you are unlikely to have lung cancer."
+        )
+    }
+
+
+@app.route("/breastcancer", methods=["POST"])
+def breastcancer():
+    data = request.get_json()
+    input_data = [
+        [
+            data["mean_radius"],
+            data["mean_texture"],
+            data["mean_perimeter"],
+            data["mean_area"],
+            data["mean_smoothness"],
+        ]
+    ]
+    inputarray = np.asarray(input_data).reshape(1, -1)
+    scaled_data, _ = scale_data_brest(inputarray)
+    prediction = brest_cancer_model.predict(scaled_data)
+    return {
+        "prediction": (
+            "Yes, you are likely to have breast cancer."
+            if prediction[0] == 1
+            else "No, you are unlikely to have breast cancer."
+        )
+    }
+
+
+@app.route("/insurance", methods=["POST"])
+def insurance():
+    data = request.get_json()
+    input_data = [
+        [
+            data["age"],
+            data["sex"],
+            data["bmi"],
+            data["children"],
+            data["smoker"],
+            data["region"],
+        ]
+    ]
+    inputarray = np.asarray(input_data).reshape(1, -1)
+    prediction = insurancemodel.predict(inputarray)
+    return {"prediction": prediction[0]}
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
