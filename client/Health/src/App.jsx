a2a8fca4fc2e0b79a83fe1614c1a0373429884aa
AryanMadkar <aryanmadkar70@gmail.com>
2025-01-23 17:10:46 +0530

    frontend staarted

---
+++ client/Health/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home count={count} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
