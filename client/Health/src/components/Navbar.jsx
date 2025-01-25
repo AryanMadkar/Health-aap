ed62e43801ed58fd4c2d44f2cf29a1ee65578c12
AryanMadkar <aryanmadkar70@gmail.com>
2025-01-26 05:14:26 +0530

    restring

--- client/Health/src/components/Navbar.jsx
+++ client/Health/src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import logo from "/logo.png";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="text-red-400  w-full flex flex-row mt-8 items-center justify-between h-[6rem]">
+      <div className="logo  ">
+        <img
+          src={logo}
+          className="h-auto  rounded-full  w-[9rem] flex items-center justify-center"
+        />
+      </div>
+      <div className="">
+        <ul className="flex shero items-center justify-center gap-2  ml-2">
+          <li>Home</li>
+          <li>About us</li>
+          <li>Prediction</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
