7b55d5030944eee35a067d770397f97cffb75431
AryanMadkar <aryanmadkar70@gmail.com>
2025-02-17 01:00:36 +0530

    testing

--- client2/src/components/Liver.jsx
+++ client2/src/components/Liver.jsx
@@ -295,20 +295,66 @@ const Liver = () => {
                   </button>
                 </motion.div>
               </AnimatePresence>}
-      <div
-        className="h-[99vh] absolute left-[-5rem] w-[25rem] bg-gradient-to-r from-[#3a0ca3] via-[#7209b7] to-[#4361ee] shadow-xl"
+              <div
+        className="h-[99vh] absolute flex items-center justify-center   left-[-5rem] w-[25rem] bg-gradient-to-r from-[#3a0ca3] via-[#7209b7] to-[#4361ee] "
         style={{ clipPath: "polygon(0 0, 100% 32%, 100% 68%, 0 100%)" }}
-      ></div>
+      >
+        <div className="w-fit h-[10rem] ml-10 bg-black flex items-center justify-center">
+          <div className="stat">
+            <div className="stat-figure text-primary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-8 w-8 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                ></path>
+              </svg>
+            </div>
+            <div className="stat-title">Model Accuracy</div>
+            <div className="stat-value text-primary">98.24%</div>
+            <div className="stat-desc">21% more than Aveage Model</div>
+          </div>
+        </div>
+      </div>
       <div
-        className="h-[99vh] absolute right-[-5rem] w-[25rem] bg-gradient-to-l from-[#3a0ca3] via-[#7209b7] to-[#4361ee] shadow-xl"
+        className="h-[99vh] absolute flex items-center justify-center  right-[-5rem] w-[25rem] bg-gradient-to-l from-[#3a0ca3] via-[#7209b7] to-[#4361ee] shadow-xl"
         style={{ clipPath: "polygon(0 32%, 100% 0, 100% 100%, 0 68%)" }}
-      ></div>
+      >
+        <div className="w-fit h-[10rem] mr-10 bg-black flex items-center justify-center">
+          <div className="stat">
+            <div className="stat-figure text-secondary">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-8 w-8 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 10V3L4 14h7v7l9-11h-7z"
+                ></path>
+              </svg>
+            </div>
+            <div className="stat-title">Page Views</div>
+            <div className="stat-value text-secondary">2.6M</div>
+            <div className="stat-desc">21% more than last month</div>
+          </div>
+        </div>{" "}
+      </div>
       <div
-        className="h-[99vh] absolute top-[-5rem] w-full bg-gradient-to-b from-[#3a0ca3] via-[#7209b7] to-[#4361ee] shadow-xl"
+        className="h-[99vh] absolute flex items-center justify-center   top-[-5rem] w-full bg-gradient-to-b from-[#3a0ca3] via-[#7209b7] to-[#4361ee] shadow-xl"
         style={{ clipPath: "polygon(0 0, 100% 0, 70% 28%, 30% 28%)" }}
       ></div>
       <div
-        className="h-[99vh] absolute bottom-[-5rem] w-full bg-gradient-to-t from-[#3a0ca3] via-[#7209b7] to-[#4361ee] shadow-xl"
+        className="h-[99vh] absolute flex items-center justify-center  bottom-[-5rem] w-full bg-gradient-to-t from-[#3a0ca3] via-[#7209b7] to-[#4361ee] shadow-xl"
         style={{ clipPath: "polygon(30% 72%, 70% 72%, 100% 100%, 0 100%)" }}
       ></div>
     </div>
